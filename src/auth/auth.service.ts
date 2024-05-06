fdcf9851067ebe2c8aee6fb1fefb9e116f1977b5
niraj <nirajnimantha@gmail.com>
2024-05-06 23:43:50 +0530

    Add JwtModule to AuthModule for token authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthUserDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signin(dto: AuthUserDto) {
     const user = await this.prisma.user.findUnique({
       where: {
@@ -54,5 +60,14 @@ export class AuthService {
         throw error;
       }
     }
+  }
+
+  async signtoken(userid: number, email: string): Promise<string> {
+    const payload = { sub: userid, email };
+    const secret = this.config.get('JWT_SECRET');
+    return this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: secret,
+    });
   }
 }
