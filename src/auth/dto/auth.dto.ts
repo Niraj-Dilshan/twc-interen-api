27fc4698271790fe454d57d4bb0b2c8ddae66bd7
niraj <nirajnimantha@gmail.com>
2024-05-06 21:23:33 +0530

    Add PrismaModule for database integration

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginUserDto {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString({ message: 'Password is required' })
+  password: string;
+}
