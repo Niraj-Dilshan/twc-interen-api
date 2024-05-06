f1ce657b324e685e351f1456e5d67a2cb7c0034f
niraj <nirajnimantha@gmail.com>
2024-05-06 15:44:38 +0530

    Add ContactController to ContactModule

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContactService } from './contact.service';
+import { ContactController } from './contact.controller';
 
 @Module({
-  providers: [ContactService]
+  providers: [ContactService],
+  controllers: [ContactController]
 })
 export class ContactModule {}
