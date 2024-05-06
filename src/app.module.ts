6ad7efb45e65ec85453edb0220df156228f9f83f
niraj <nirajnimantha@gmail.com>
2024-05-06 19:05:00 +0530

    Add AuthModule to app imports and update npm dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ContactModule } from './contact/contact.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ContactModule],
+  imports: [UserModule, ContactModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
