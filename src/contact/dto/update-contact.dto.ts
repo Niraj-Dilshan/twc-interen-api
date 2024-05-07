7fb4f99813965d76b0dd8358d0483d68f20b7e69
niraj <nirajnimantha@gmail.com>
2024-05-07 12:02:47 +0530

    Update contact DTOs to make fields optional

--- src/contact/dto/update-contact.dto.ts
+++ src/contact/dto/update-contact.dto.ts
@@ -4,19 +4,19 @@ import { IsEmail, IsOptional, IsString, Matches } from 'class-validator';
 export class UpdateContactDto {
   @IsOptional({ message: 'Name is required' })
   @IsString({ message: 'Name can only contain A-Z, a-z,., and spaces' })
-  name: string;
+  name?: string;
 
   @IsOptional({ message: 'Email is required' })
   @IsEmail({}, { message: 'Invalid email format' })
-  email: string;
+  email?: string;
 
   @IsOptional({ message: 'Phone number is required' })
   @Matches(/^\d{10}$|^\+\d{1,2}\s?\d{9}$/, {
     message: 'Invalid phone number format',
   })
-  phone: string;
+  phone?: string;
 
   @IsOptional({ message: 'Gender is required' })
   @IsString({ message: 'Gender must be male or female' })
-  gender: 'male' | 'female';
+  gender?: 'male' | 'female';
 }
