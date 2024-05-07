c7384a621ad2c92ebbb90df52445c96aa84767c1
niraj <nirajnimantha@gmail.com>
2024-05-07 10:19:01 +0530

    Update ContactController to use plural endpoint name and add GET method for retrieving all contacts

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('contacts')
 export class ContactController {
-  @UseGuards(JwtGuard)
   @Get()
-  getContacts() {
-    return 'All contacts';
+  getContacts(@GetUser('id') userID: number) {
+    return userID;
   }
 }
