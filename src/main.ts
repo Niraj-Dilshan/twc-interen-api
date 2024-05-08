83f23efab238d1e5ddab8ff3fb1273d6e7f1c0af
niraj <nirajnimantha@gmail.com>
2024-05-08 09:49:39 +0530

    enable CORS globally in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS globally
+  app.enableCors({
+    origin: '*', // Allow all origins for development purposes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
   await app.listen(3001);
 }
 bootstrap();
